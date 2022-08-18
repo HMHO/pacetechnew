96b99b6b4643c493ee46cd838aa2e40f6a5bb3a0
Mauricio Herrera <hermaho7@hotmail.com>
2022-08-17 22:48:14 -0300

    Adición de clase conversora

---
+++ src/main/java/Conversor.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Conversor {
+    // esta es la nueva clase conversora
+}
